0111fd06ecbcedc96a24a2954df571f2ec7b9038
tuppudu sindhu <162948507+TuppuduSindhu@users.noreply.github.com>
2025-05-05 09:54:40 +0530

    user input .java

--- input_output/user input .java
+++ input_output/user input .java
@@ -1,1 +1,18 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("hiiiiiiiiiiiiiiii");
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      
+      String ch=sc.next();
+      long a=sc.nextLong();
+      char b=sc.next().charAt(0);
+      System.out.println(n);
+      System.out.println(ch);
+      System.out.println(a);
+      System.out.println(b);
+      
+  }
+}
