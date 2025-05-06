2dc2fa19c534ae774492b66a8bdb442300117bcf
tuppudu sindhu <162948507+TuppuduSindhu@users.noreply.github.com>
2025-05-06 06:38:20 +0530

    methods.java

--- methods/methods.java
+++ methods/methods.java
@@ -1,1 +1,33 @@
+//example 1
 
+import java.util.*;
+public class Main{
+  public static void printing(){
+    System.out.println("mesg printted ");
+  }
+  public static void main(String[] args){
+    printing();
+  }
+}
+
+
+
+//example 2
+import java.util.*;
+
+public class Main {
+    public static void hello() {
+        System.out.println("helloo all");
+    }
+
+    public static int add(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum); 
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        hello();
+        add(3, 4);
+    }
+}
