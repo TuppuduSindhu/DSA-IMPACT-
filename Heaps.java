e7735e06c122c88b9832bd1407f705f381264038
tuppudu sindhu <162948507+TuppuduSindhu@users.noreply.github.com>
2025-06-05 08:49:02 +0530

    Heaps.java

---
+++ Heaps.java
@@ -0,0 +1,19 @@
+=>Heap data Structure will store the data in arrays it will be stored by trees and
+  ->the values will be arranged by trees with the help of complete binary tree 
+  ->with the help of priority queues heaps got created 
+  ->Heaps are created with two datastructures they are trees and arrays 
+  ->elements can access through index values and arranged in tree structure of trees
+  ->Types : 1.bottum to top approach 
+            2.top to botttum approach 
+
+
+
+  =========================N-Arrays ===========================
+  Tries : we have to create a node which can store character array  of 26 alphabets .
+  ->the last node need to change the flag to true 
+  ->structure : class Node{
+  int[26]data 
+  boolean flag;
+  Node next;
+}
+Advantage : to get the faster retrivals of the strings 
